dac4cc538c115ec6eacb0ad247df249d6951140e
DexMP <matveymp@yandex.ru>
2023-10-03 16:05:31 +0300

    Новый цикл с добавлением продуктов

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,7 +1,14 @@
 public class Calculator {
 
     double summ;
+    int a;
+    int b;
 
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+        this.summ = sum(a, b);
+    }
 
     public static int sum(int a, int b) {
         return a + b;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // Переменные
+        int humans;
+        double price;
+        String products = null;
+        float summ;
+
+        // Сканеры
+        Scanner scanner_humans = new Scanner(System.in);
+        Scanner scanner_products = new Scanner(System.in);
+
         // Поздороваемся!
         System.out.println("Доброго времени суток!\n" +
                 "На скольких человек необходимо разделить счёт:");
-
-        // Переменная хранящая людей (да-да как криокамера)
-        int humans;
-
-        // Сканер
-        Scanner scanner_humans = new Scanner(System.in);
 
         // Бесконечный цикл который позволяет нам не вывалиться из программы и спрашивать пользователя количество людей
         while (true) {
@@ -20,11 +24,7 @@ public class Main {
                 while (humans <= 1) {
                     System.out.println("Количество людей должно быть более 1");
                     humans = scanner_humans.nextInt();
-                    if (humans > 1) {
-                        break;
-                    } else {
-                        System.out.println("Введено не число!");
-                    }
+                    if (humans > 1) break;
                 }
                 break;
             } else {
@@ -33,8 +33,32 @@ public class Main {
             }
         }
 
-
-
         System.out.println(humans);
+
+        System.out.println("Что вы заказывали:");
+
+        while (true) {
+            if (scanner_products.hasNext()) {
+                // Название
+                products = scanner_products.nextLine();
+                System.out.println("Сколько стоит " + products + ":");
+                while (true) {
+                    if (scanner_products.hasNext()) {
+                        // Ценник
+                        price = scanner_products.nextDouble();
+                        Product product1 = new Product(products, price);
+                        System.out.println(product1.productName + " = " + product1.productPrice);
+                        break;
+                    } else {
+                        System.out.println("Кого ты хочешь обмануть? Вводи сумму:");
+                        price = scanner_products.nextDouble();
+                    }
+                }
+                break;
+            } else {
+                System.out.println("Кажется строка пустая! Попробуйте ещё раз:");
+                products = scanner_products.nextLine();
+            }
+        }
     }
 }
