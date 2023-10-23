22911ef1840599fc969ba13614dab9b0af13dcc5
DexMP <matveymp@yandex.ru>
2023-10-23 18:54:12 +0300

    mini fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,21 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // Classes
+        Product product = null;
         // Переменные
         int humans;
-        double price;
-        String products = null;
+        double price = 0;
+        String products;
         float summ;
 
         // Сканеры
         Scanner scanner_humans = new Scanner(System.in);
         Scanner scanner_products = new Scanner(System.in);
+        Scanner scanner_prices = new Scanner(System.in);
 
         // Поздороваемся!
         System.out.println("Доброго времени суток!\n" +
                 "На скольких человек необходимо разделить счёт:");
 
-        // Бесконечный цикл который позволяет нам не вывалиться из программы и спрашивать пользователя количество людей
+        // Получаем количество людей
         while (true) {
             if (scanner_humans.hasNextInt()) {
                 humans = scanner_humans.nextInt();
@@ -34,30 +37,43 @@ public class Main {
         }
 
         System.out.println(humans);
-
-        System.out.println("Что вы заказывали:");
-
+        System.out.println("Что было в заказе:");
+        // Получаем товары
         while (true) {
+            // Наименование
             if (scanner_products.hasNext()) {
-                // Название
                 products = scanner_products.nextLine();
-                System.out.println("Сколько стоит " + products + ":");
-                while (true) {
-                    if (scanner_products.hasNext()) {
-                        // Ценник
-                        price = scanner_products.nextDouble();
-                        Product product1 = new Product(products, price);
-                        System.out.println(product1.productName + " = " + product1.productPrice);
+                while (products.isEmpty()) {
+                    System.out.println("Нет данных");
+                    products = scanner_products.nextLine();
+                    if (!products.isEmpty()) {
                         break;
-                    } else {
-                        System.out.println("Кого ты хочешь обмануть? Вводи сумму:");
-                        price = scanner_products.nextDouble();
                     }
                 }
-                break;
+
+                System.out.println("Цена товара в формате 'рубли.копейки':");
+
+                // Цена
+                if (scanner_prices.hasNextDouble()) {
+                    price = scanner_prices.nextDouble();
+                    while (price <= 1) {
+                        System.out.println("Значение не может быть меньше 1");
+                        price = scanner_products.nextDouble();
+                        if (price > 1) {
+                            product = new Product(products, price);
+                            break;
+                        }
+                    }
+                } else {
+                    System.out.println("Неверный формат записи");
+                    scanner_prices.hasNextDouble();
+                }
             } else {
-                System.out.println("Кажется строка пустая! Попробуйте ещё раз:");
-                products = scanner_products.nextLine();
+                System.out.println("Не удалось прочитать, попробуйте снова");
+                scanner_products.nextLine();
+            }
+            if (product != null) {
+                System.out.println("Наиименование" + product.productName + "\nЦена: " + product.productPrice);
             }
         }
     }
